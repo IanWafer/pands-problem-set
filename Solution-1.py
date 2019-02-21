7662323bb882c302fe4b96290cb3ee87c38ae973
Ian Wafer <ianwafer@gmail.com>
2019-02-21 20:38:34 +0000

    First attempt of problem 1. Formula to be rectified.

--- Solution-1.py
+++ Solution-1.py
@@ -1,1 +1,12 @@
+# Ian Wafer 21-02-2019
 # Solution to problem 1
+
+x = int(input("Please enter a positive integer: "))  
+ans = 1
+i = 1
+
+while i <= x:
+    ans= ans + i
+    i = i + 1
+
+print(ans)  
