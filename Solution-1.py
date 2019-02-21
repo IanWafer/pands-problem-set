6e263e27c7f93785ea9880013a6d496215ec2f46
Ian Wafer <ianwafer@gmail.com>
2019-02-21 21:36:48 +0000

    Finished problem 1

--- Solution-1.py
+++ Solution-1.py
@@ -1,12 +1,16 @@
 # Ian Wafer 21-02-2019
 # Solution to problem 1
+# Calculate the integers between the input number and 0
 
-x = int(input("Please enter a positive integer: "))  
-ans = 1
-i = 1
+x = int(input("Please enter a positive integer: "))  # User must input a value to sum up
+ans = 0                                              # This will be used to sum up the correct figures and print the answer
+i = 0                                                # This  ensures the first iteration is at 0 making the initial value 1 included 
 
-while i <= x:
-    ans= ans + i
-    i = i + 1
+if x <= 0:                                           # Value needs to be positive to create sum
+   x = int(input("Negative numbers not accepted. Please enter a positive integer: ")) # Reminder for a positive value is required
 
-print(ans)  
+while i <= x:                                        # This ensures the value of i does not inrease past the value of the input figure resulting in a loop 
+    ans = ans + i                                    # Sum of iterations
+    i = i + 1                                        # Move to next iteration until while condition not met
+
+print(ans)                                           # When while condition not met print ans value
