255c62f328d8afd811971436a7c7ac5a6e594534
Ian Wafer <ianwafer@gmail.com>
2019-02-24 16:39:04 +0000

    Solution 4 code corrected

--- Solution-4.py
+++ Solution-4.py
@@ -3,10 +3,10 @@
 # Output succesive values of input number. At each step calculate the next value by taking the current value and if even divide it by two but if odd multiply it by 3 and add one. Have the program end if the current value is one.
 
 x = int(input("Please enter a positive integer: "))  # User must input a value to beign solution
+print(x)
 
 while x > 1:
   if x % 2 == 0:
     x = x // 2
-    print(x)
   else: x = (x * 3) + 1
   print(x)
