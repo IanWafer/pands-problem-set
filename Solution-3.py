404c4b8c3ef3a49e0944fb72c653a2f039b2bd2c
Ian Wafer <ianwafer@gmail.com>
2019-02-24 14:26:34 +0000

    Comments added to solution 3

--- Solution-3.py
+++ Solution-3.py
@@ -1,13 +1,13 @@
-# Ian Wafer 23-02-2019
+# Ian Wafer 24-02-2019
 # Solution to problem 3
 # Print all numbers between 1,000 and 10,000 that are divisible by 6 but not 12
 
-a=1000
+a=1000                      # Define starting variable
 
-while 1000 <= a <= 10000:
-  if a % 12 == 0:
-    a = a + 1
-  if a % 6 == 0: 
-    print(a)  
-    a = a + 1
-  else: a = a + 1
+while 1000 <= a <= 10000:   # Number must be between 1,000 and 10,000
+  if a % 12 == 0:           # Check if function is divisible by 12 with no remainder
+    a = a + 1               # If condition is true change a value to a + 1
+  if a % 6 == 0:            # Check if function is divisible by 6 with no remainder
+    print(a)                # If above function true print a
+    a = a + 1               # After printing a value above the next iteration becomes a + 1
+  else: a = a + 1           # If neither condition above met add one to a value and run script above again
