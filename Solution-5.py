26743f2e1b1989f834db9cfe928155a2d78c61c4
Ian Wafer <ianwafer@gmail.com>
2019-03-02 17:23:03 +0000

    Comments added solution-5

--- Solution-5.py
+++ Solution-5.py
@@ -6,10 +6,10 @@ x = int(input("Please enter a positive integer: "))  # User must input a value t
 
 if x > 1:                                            # x must be a positive value greater than 1 to determine if it is prime.
 
-  for i in range(2,x):
-    if (x % i) == 0:
-      print("This is not a prime number") 
-      break
+  for i in range(2,x):                               # Check for within range of 2 to x
+    if (x % i) == 0:                                 # Check if x divided by i value leaves a remainder
+      print("This is not a prime number")            # If there is no remainder in formula on line above then x is not a prime. Print string
+      break                                          # Once x is first determined to not be prime end algorithm. This removes the need to run through all possible calculations as we know it's not a prime numnber witha single confirmation.
     
 
-  else:print("This is a prime number")
+  else:print("This is a prime number")                # When If function not met then x must be prime. Print string. 
