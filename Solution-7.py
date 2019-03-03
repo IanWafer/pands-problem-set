37560e7513d68a3067f1b6ab435af61c0ad46b2d
Ian Wafer <ianwafer@gmail.com>
2019-03-03 21:01:04 +0000

    Solution-8 requires amending

--- Solution-7.py
+++ Solution-7.py
@@ -2,10 +2,9 @@
 # Solution to problem 7
 # Output approximate square root of an input positive floating point number
 
-import math
+import math                                           # Import the math module for the square root function
 
 x = float(input("Please enter a positive number: "))  # User must input a value to begin solution
-y=math.sqrt(x)
+y=math.sqrt(x)                                        # Make y value the square root of x
 
-
-print("The square root of %f is approx. %.1f." % (x,y))
+print("The square root of %f is approx. %.1f." % (x,y)) # Print text with the %f function being a floating number and %.1f being the floating number to 1 decimal point.
--- Solution-8.py
+++ Solution-8.py
@@ -4,5 +4,17 @@
 
 import time                                             # import the time module
 
-print(time.strftime("%A, %B %d %Y at %H:%M"))
 
+if time.strftime("%d") == 1 or 21 or 31:
+    q = "st"
+
+if time.strftime("%d") == 2 or 22:
+    q = "nd"
+
+if time.strftime("%d") == 3 or 23:
+    q == "rd"
+
+if time.strftime("%d") == 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11 or 12 or 13 or 14 or 15 or 16 or 17 or 18 or 19 or 20 or 24 or 25 or 26 or 27 or 28 or 29 or 30:
+    q == "th"
+
+print(time.strftime("%A, %B %d %Y at %H:%M%p"))
