2e59e4b5798aeaf61274708f0321394416cdb648
Ian Wafer <ianwafer@gmail.com>
2019-03-03 17:49:46 +0000

    Attempted solution to solution 6

--- Solution-6.py
+++ Solution-6.py
@@ -2,8 +2,7 @@
 # Solution to problem 6
 # Output every second word of user string input
 
-x = str(input("Please enter a sentence: "))
+x = input("Please enter a sentence: ")
 
-x = [x]
-
-print(range(0,x,2))
+for word in x.split():
+  print(word)
