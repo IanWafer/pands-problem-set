6524693e466cac3f66360c04b65f2142eeb4bec7
Ian Wafer <ianwafer@gmail.com>
2019-03-03 20:16:10 +0000

    Solution 8 mostly complete. Day not yet correct

---
+++ Solution-8.py
@@ -0,0 +1,8 @@
+# Ian Wafer 03-03-2019
+# Solution to problem 8
+# Display the current date and time in the format "Monday, January 10th 2019 at 1:15pm".
+
+import time                                             # import the time module
+
+print(time.strftime("%A, %B %d %Y at %H:%M"))
+
