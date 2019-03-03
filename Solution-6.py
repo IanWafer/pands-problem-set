73aa99def027aa37a0d427a351a5cfc69ec58f5e
Ian Wafer <ianwafer@gmail.com>
2019-03-03 19:20:39 +0000

    Solution-6.py complete

--- Solution-6.py
+++ Solution-6.py
@@ -2,8 +2,8 @@
 # Solution to problem 6
 # Output every second word of user string input
 
-x = input("Please enter a sentence: ")
+x = input("Please enter a sentence: ") # User must input a string to begin solution
 
-y = x.split(" ")
+y = x.split(" ")                       # Split string into a list with each word as a seperate item. Seperate wods identified by a " " (space) between them
 
-print(y[1::2])
+print(" ".join(y[0::2]))               # From list y obtained above starting at the initial point (0) make a new list in increments of 2 till the end of the list. Join these values together seperated by a " " (space) and print output.  
