4d8f597431f39b737a0ef164cb5e3cdba165e0ba
Ian Wafer <ianwafer@gmail.com>
2019-03-14 20:33:00 +0000

    Comments added to solution-9

--- Solution-8.py
+++ Solution-8.py
@@ -15,6 +15,6 @@ if time.strftime("%d") == 3 or 23:                      # if string format shows
     q = "rd"
     
 if time.strftime("%d") == 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11 or 12 or 13 or 14 or 15 or 16 or 17 or 18 or 19 or 20 or 24 or 25 or 26 or 27 or 28 or 29 or 30:
-    q = "th"                                             # if string format shows these numbers create q string as th
+    q = "th"                                            # if string format shows these numbers create q string as th
 
 print(time.strftime(f"%A, %B %d{q} %Y at %H:%M%p" ))    # Using f" fucntion with strftime functions print out date with day suffix contained in q variable
--- Solution-9.py
+++ Solution-9.py
@@ -2,6 +2,6 @@
 # Solution to problem 9
 # Write a program to read a text file and output every second line.
 
-with open("Simple.txt", "r") as f:
-    x = f.readlines()
-    print("".join(x[0::2]))
+with open("Simple.txt", "r") as f:  # Open the file and close once indented code is finished
+    x = f.readlines()               # make value x equal to each line of the file
+    print("".join(x[0::2]))         # Print every second line. This is done by joining to gether the list created in the function starting at 0 and increasing in increments of 2
