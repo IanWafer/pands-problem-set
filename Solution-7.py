56505760c7b7040f57c8e1dd6022cb3330065aa3
Ian Wafer <ianwafer@gmail.com>
2019-03-14 19:31:48 +0000

    Made solution-7 simpler

--- Solution-7.py
+++ Solution-7.py
@@ -5,6 +5,5 @@
 import math                                           # Import the math module for the square root function
 
 x = float(input("Please enter a positive number: "))  # User must input a value to begin solution
-y=math.sqrt(x)                                        # Make y value the square root of x
 
-print("The square root of %f is approx. %.1f." % (x,y)) # Print text with the %f function being a floating number and %.1f being the floating number to 1 decimal point.
+print("The square root of %f is approx. %.1f." % (x,math.sqrt(x))) # Print text with the %f function being a floating number and %.1f being the floating number to 1 decimal point.
