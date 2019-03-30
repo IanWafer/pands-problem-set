e760b7132cdd20cccac8e138f562c8ea17d85520
Ian Wafer <ianwafer@gmail.com>
2019-03-30 19:52:40 +0000

    Solution 10 complete. Comments to be added

--- Solution-10.py
+++ Solution-10.py
@@ -1,10 +1,24 @@
 # Ian Wafer 24-03-2019
 # Solution to problem 10
-# Write a program that displays a plot of the functions x, x2 and 2^x in the range [0,4]
+# Write a program that displays a plot of the functions x, x^2 and 2^x in the range [0,4]
+# Source https://docs.scipy.org/doc/numpy/reference/generated/numpy.power.html
+# Source https://matplotlib.org/users/pyplot_tutorial.html
+# Source https://jakevdp.github.io/PythonDataScienceHandbook/04.06-customizing-legends.html
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([0,1,2,3,4])
+x1 = np.arange(0., 4., 0.05)
+x2 = np.power(x1, 2)
+x3 = np.power(2, x1)
+
+fig, ax = plt.subplots()
+ax.plot(x1, x1, 'r--', label='x')
+ax.plot(x1, x2, 'bs', label='x^2')
+ax.plot(x1, x3, 'g^', label='2^x')
+ax.legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+plt.title('Plot of the functions x, x^2 and 2^x in the range [0,4]')
 
 plt.show()
